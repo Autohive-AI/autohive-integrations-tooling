2dc5889c0642db8fcc31218d200860c092dfe64d
Shubhank <72601061+Sagsgit@users.noreply.github.com>
2026-01-23 13:57:48 +1300

    Add bad integration for testing

---
+++ Bad-Integration/bad_integration.py
@@ -0,0 +1,12 @@
+from autohive_integrations_sdk import (
+    Integration,
+    ExecutionContext,
+    ActionHandler,
+    ActionResult
+)
+
+# Syntax error - missing closing parenthesis
+def broken_function(:
+    return "this is broken"
+
+bad_integration = Integration.load()
